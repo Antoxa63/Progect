238ad0217afbcc9ea477927240151eae739c7e02
Antoxxa63 <63larionov@gmail.com>
2022-02-20 18:44:47 +0400

    fix lodash

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 import _ from "lodash";
 
-const makeCensored = (text1, text2) => {
-   const newText1 = Array.from(new Set(text1));
-   const newText2 = Array.from(new Set(text2));
-   let result = 0
-   for (let i = 0; i < newText1.length; i++) {
-      for (let y = 0; y < newText2.length; y++) {
-         if (newText1[i] === newText2[y])
-            result = result + 1
-      }
-   }
-   return result
-};
-console.log(makeCensored([0], ['one', 'two']));
+console.log(_.last(['one', 'two']));
